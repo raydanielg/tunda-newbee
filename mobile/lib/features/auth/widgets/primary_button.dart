e6f7cc888f7d1f8dce0682fac4ddf410fdb19487
raydanielg <airezra2+1@gmail.com>
2026-07-23 21:28:29 +0300

    commit

---
+++ mobile/lib/features/auth/widgets/primary_button.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import '../../../core/constants/app_colors.dart';
+
+class PrimaryButton extends StatefulWidget {
+  final String label;
+  final IconData icon;
+  final VoidCallback? onPressed;
+  final bool loading;
+
+  const PrimaryButton({
+    super.key,
+    required this.label,
+    required this.icon,
+    this.onPressed,
+    this.loading = false,
+  });
+
+  @override
+  State<PrimaryButton> createState() => _PrimaryButtonState();
+}
+
+class _PrimaryButtonState extends State<PrimaryButton> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: widget.loading ? null : widget.onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.maroon400,
+          foregroundColor: AppColors.white,
+          elevation: 4,
+          shadowColor: AppColors.maroon400.withOpacity(0.3),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        child: widget.loading
+            ? const SizedBox(
+                width: 20,
+                height: 20,
+                child: CircularProgressIndicator(
+                  strokeWidth: 2.5,
+                  valueColor: AlwaysStoppedAnimation(AppColors.white),
+                ),
+              )
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(widget.icon, size: 20),
+                  const SizedBox(width: 8),
+                  Text(widget.label),
+                ],
+              ),
+      ),
+    );
+  }
+}
