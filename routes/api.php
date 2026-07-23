ca662c3b2dca7ecfe4f8776ffb8c4aee881473ab
raydanielg <airezra2+1@gmail.com>
2026-07-23 21:36:39 +0300

    commit

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('auth/user', [AuthController::class, 'user']);
+    Route::post('auth/logout', [AuthController::class, 'logout']);
+});
